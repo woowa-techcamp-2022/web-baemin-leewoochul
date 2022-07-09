f5a5194b71fd61ca61602e2d827b91ca9267b453
woochul2 <leewoo0686@gmail.com>
2022-07-10 01:12:43 +0900

    feat: 비밀번호 3번 반복되는 숫자 유효성 검증

--- public/javascripts/utils/validate.js
+++ public/javascripts/utils/validate.js
@@ -1,24 +1,38 @@
 // 출처: https://html.spec.whatwg.org/multipage/input.html#email-state-(type=email)
-export function validateEmail(value) {
-  return /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(
+export const validateEmail = (value) =>
+  /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(
     value
   );
-}
 
-export function validateNickname(nickname) {
-  return nickname.length >= 2 && nickname.length <= 10;
+export const validateNickname = (nickname) =>
+  nickname.length >= 2 && nickname.length <= 10;
+
+const hasThreeRepeatedNumber = (number) => /([0-9])\1{2,}/.test(number);
+
+function hasSeqentialNumber(value) {
+  for (let i = 0; i < value.length - 2; i++) {
+    const subString = value.slice(i, i + 3);
+    if ('0123456789'.includes(subString) || '9876543210'.includes(subString)) {
+      return true;
+    }
+  }
+  return false;
 }
 
 export function validatePassword(value) {
-  if (value.length < 10) return false;
+  const MAX_LENGTH = 10;
+  if (value.length < MAX_LENGTH) return false;
 
   let caseCounts = 0;
   if (/[A-Z]/.test(value)) caseCounts++;
   if (/[a-z]/.test(value)) caseCounts++;
   if (/[0-9]/.test(value)) caseCounts++;
   if (/[!@#\$%\^\&*\)\(+=._-]/.test(value)) caseCounts++;
-  if (caseCounts < 2) return false;
+  const MIN_CASE_COUNTS = 2;
+  if (caseCounts < MIN_CASE_COUNTS) return false;
 
+  if (hasThreeRepeatedNumber(value)) return false;
+  if (hasSeqentialNumber(value)) return false;
   return true;
 }
 
