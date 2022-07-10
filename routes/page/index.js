e1739a52631b01dbe0d5214fa443c1e3328b1be8
woochul2 <leewoo0686@gmail.com>
2022-07-10 16:26:08 +0900

    feat: 휴대전화 페이지 헤더 추가

--- routes/page/index.js
+++ routes/page/index.js
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import homeRouter from './home.js';
 import loginRouter from './login.js';
 import agreementRouter from './agreement.js';
+import phoneRouter from './phone.js';
 import signupRouter from './signup.js';
 const router = Router();
 
 router.use('/', homeRouter);
 router.use('/login', loginRouter);
 router.use('/agreement', agreementRouter);
+router.use('/phone', phoneRouter);
 router.use('/signup', signupRouter);
 
 export default router;
